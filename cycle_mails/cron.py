85c74b98628bcba3b4c2a45ca19ab5934b153eb9
Kurara <clara.pena@infojuice.eu>
2020-08-11 20:30:30 +0200

    added email sent by gmail

--- cycle_mails/cron.py
+++ cycle_mails/cron.py
@@ -2,6 +2,7 @@ from cycle_mails.models import Crontab, SentDone
 from django.utils import timezone
 import datetime
 import logging
+from django.core.mail import send_mail
 
 
 logger = logging.getLogger(__name__)
@@ -23,11 +24,11 @@ def check_mails_tosend():
     for cron in _once:
         last_sent = SentDone.objects.filter(crontab=cron)
         if not last_sent:
-            send_mail(cron)
-        # We use the margin of 5 minutes of cron
+            prepare_mail(cron)
+        # We mail_queue the margin of 5 minutes of cron
         elif not (last_sent.minute > now.minute -5 and \
             last_sent.minute < now.minute +5):
-            send_mail(cron)
+            prepare_mail(cron)
 
     # Check weekly send
     _weekly = Crontab.objects.filter(
@@ -39,14 +40,14 @@ def check_mails_tosend():
     for cron in _weekly:
         last_sent = SentDone.objects.filter(crontab=cron)
         if not last_sent:
-            send_mail(cron)
+            prepare_mail(cron)
         # last sent past week. isocalendar gives week number in year
         elif last_sent.isocalendar()[1] < now.isocalendar()[1]:
-            send_mail(cron)
+            prepare_mail(cron)
         # last sent in last year (the week should be the same
         # as filter is week=now)
         elif last_sent.year < now.year:
-            send_mail(cron)
+            prepare_mail(cron)
 
     # Check monthly send
     _monthly = Crontab.objects.filter(
@@ -58,15 +59,28 @@ def check_mails_tosend():
     for cron in _monthly:
         last_sent = SentDone.objects.filter(crontab=cron)
         if not last_sent:
-            send_mail(cron)
+            prepare_mail(cron)
         # last sent past month
         elif last_sent.month < now.month:
-            send_mail(cron)
+            prepare_mail(cron)
         # last sent in last year (the month should be the same
         # as filter is month=now)
         elif last_sent.year < now.year:
-            send_mail(cron)
+            prepare_mail(cron)
 
 
-def send_mail(cron):
+def prepare_mail(cron):
     logger.info("Sending mail for reunion <{}>...".format(cron.reunion))
+
+    content = cron.reunion.content_set.all().first()
+
+    emails = list(map(lambda e: e.name, cron.reunion.emails.all()))
+
+    if content:
+        send_mail(
+            content.subject,
+            content.body,
+            'andujar@envios_reuniones.es',
+            emails,
+            fail_silently=False,
+        )
--- envio_reuniones/settings.py
+++ envio_reuniones/settings.py
@@ -133,3 +133,10 @@ CRONJOBS = [
     # Each 5 minutes
     ('*/5 * * * *', 'cycle_mails.cron.check_mails_tosend')
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "your_account@gmail.com"
+EMAIL_HOST_PASSWORD = "your account's password"
